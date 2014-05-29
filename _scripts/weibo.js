dfb8afcc0e7f99985f16d20c93e557abc021d034
Tsuhui <notifies@163.com>
2014-05-30 01:06:18 +0800

    PHP
    add PHP index file for Heroku

--- _scripts/weibo.js
+++ _scripts/weibo.js
@@ -14,7 +14,7 @@ XPlusZ.Configs = {};
 XPlusZ.Configs.Weibo = {
     Count: 200,
     PageSize: 10,
-    SerectKey: '######################################',
+    SerectKey: '2.008GgngBchRtjC12008e2ca0vcdA3C',
     TimeLineAPI: 'https://api.weibo.com/2/statuses/public_timeline.json'
 };
 
